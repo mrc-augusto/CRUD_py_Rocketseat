44f284b15138b6d82c1bff74569745921812477e
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-06-26 15:43:37 -0300

    Get task by Id method created

--- models/task.py
+++ models/task.py
@@ -1,6 +1,6 @@
 class Task:
   def __init__(self, id, title, description, completed=False)->None:
-    self.id = id,
+    self.id = id
     self.title = title
     self.description = description
     self.completed = completed
