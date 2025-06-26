6a57cc8659d889e23f66619626c3a9443777f174
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-06-26 15:08:03 -0300

    POST and GET Routes created

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+
+app = Flask(__name__)
+
+tasks = []
+task_id_control = 1
+
+#Create a new Task
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  global task_id_control
+  data = request.get_json()
+  new_task = Task(
+    id=task_id_control, 
+    title=data['title'], 
+    description=data.get('description', ' ')
+  )
+  task_id_control += 1
+  tasks.append(new_task)
+  print(tasks)
+  return jsonify({'message': 'Nova tarefa criada com sucesso'})
+
+#Get all Tasks
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  task_list=[task.to_dict() for task in tasks]
+  output={
+    'tasks':  task_list,
+    'total_tasks': 0
+  }  
+  return jsonify(output)
+
+if  __name__ =='__main__':
+  app.run(debug=True)
